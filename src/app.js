5c10d84b180ae04469f0757b75e7849f09f79b7f
Pierce Nylon <piercenylon@gmail.com>
2018-10-27 13:00:30 -0700

    Setup firebase and tests, fetch and display data.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { startSetExpenses, removeExpense, editExpense } from './actions/expenses';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
@@ -36,5 +36,10 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
 
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -66,3 +66,12 @@ test('should not edit an expense if the id is not found', () => {
     //expect(state[0].note).not.toBe(note);
     expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});
